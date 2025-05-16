1e23188c8e184f75f914501a9797cc6cc0876d5b
AutomationWithPiyush <automation.with.piyush@gmail.com>
2025-05-16 10:56:29 +0530

    Practice

--- A9_Basic/src/wd_methods/FirstLine.java
+++ A9_Basic/src/wd_methods/FirstLine.java
@@ -9,17 +9,15 @@ public class FirstLine {
 	public static void main(String[] args) {
 //		Explaining the first line of code
 		WebDriver driver = new ChromeDriver();
+		driver = new FirefoxDriver();
+		driver = new EdgeDriver();
+
 		/*
-		 * webdriver is an interface
-		 * driver is a reference variable
-		 * new is a keyword which will create random memory space
-		 * in heap area
-		 * chromedriver() is a constructor call which will
-		 * load , regirster and reinitialize the non static members
-		*/		
+		 * webdriver is an interface driver is a reference variable new is a keyword
+		 * which will create random memory space in heap area chromedriver() is a
+		 * constructor call which will load , regirster and reinitialize the non static
+		 * members
+		 */
 
-			driver = new FirefoxDriver();
-
-			driver = new EdgeDriver();
 	}
 }
